f05e779402efe44c1c7dec310b4bade815e7c0e4
alex sync <koghuxov1990@gmail.com>
2026-01-10 04:54:07 +0300

    storage

---
+++ storage/path.go
@@ -0,0 +1,56 @@
+package storage
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"path/filepath"
+	"strings"
+)
+
+type ResolvedPath struct {
+	PathName string
+	FileName string
+}
+
+func (p ResolvedPath) FullPath() string {
+	return filepath.Join(p.PathName, p.FileName)
+}
+
+func (p ResolvedPath) PathByIndex(pathIndex int) string {
+	paths := strings.Split(p.PathName, string(filepath.Separator))
+
+	pathLen := len(paths)
+
+	if pathLen == 0 || pathIndex > pathLen {
+		return ""
+	}
+
+	return paths[pathIndex]
+}
+
+// PathGenerator function defines the strategy for generating filepath.
+type PathGenerator func(key string) ResolvedPath
+
+// DefaultPathGenerator create path base on SHA256 hash.
+//
+// Example: hash a1b432... => a1/b4/32/.../file.svg
+func DefaultPathGenerator(key string) ResolvedPath {
+	hash := sha256.Sum256([]byte(key))
+	hashString := hex.EncodeToString(hash[:])
+
+	pathSegmentLength := 2
+	pathDepth := len(hashString) / pathSegmentLength
+	paths := make([]string, pathDepth)
+
+	for i := range pathDepth {
+		from := i * pathSegmentLength
+		to := from + pathSegmentLength
+
+		paths[i] = hashString[from:to]
+	}
+
+	return ResolvedPath{
+		PathName: strings.Join(paths, string(filepath.Separator)),
+		FileName: hashString,
+	}
+}
--- storage/store.go
+++ storage/store.go
@@ -1,13 +1,10 @@
 package storage
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/someerror/destore/core"
 )
@@ -20,6 +17,9 @@ type StoreConf struct {
 type Store struct {
 	StoreConf
 }
+
+// Check implementation of Store interface
+var _ core.Store = (*Store)(nil)
 
 // os.Stat Syscall
 func (s *Store) Has(key string) (bool, error) {
@@ -51,9 +51,6 @@ func (s *Store) Clear() error {
 	return os.RemoveAll(s.Root)
 }
 
-// Check implementation of Store interface
-var _ core.Store = (*Store)(nil)
-
 func NewStore(conf StoreConf) *Store {
 	if len(conf.Root) == 0 {
 		conf.Root = "destore_storage"
@@ -64,52 +61,4 @@ func NewStore(conf StoreConf) *Store {
 	}
 
 	return &Store{StoreConf: conf}
-}
-
-type ResolvedPath struct {
-	PathName string
-	FileName string
-}
-
-func (p ResolvedPath) FullPath() string {
-	return filepath.Join(p.PathName, p.FileName)
-}
-
-func (p ResolvedPath) PathByIndex(pathIndex int) string {
-	paths := strings.Split(p.PathName, string(filepath.Separator))
-
-	pathLen := len(paths)
-
-	if pathLen == 0 || pathIndex > pathLen {
-		return ""
-	}
-
-	return paths[pathIndex]
-}
-
-// PathGenerator function defines the strategy for generating filepath.
-type PathGenerator func(key string) ResolvedPath
-
-// DefaultPathGenerator create path base on SHA256 hash.
-//
-// Example: hash a1b432... => a1/b4/32/.../file.svg
-func DefaultPathGenerator(key string) ResolvedPath {
-	hash := sha256.Sum256([]byte(key))
-	hashString := hex.EncodeToString(hash[:])
-
-	pathSegmentLength := 2
-	pathDepth := len(hashString) / pathSegmentLength
-	paths := make([]string, pathDepth)
-
-	for i := range pathDepth {
-		from := i * pathSegmentLength
-		to := from + pathSegmentLength
-
-		paths[i] = hashString[from:to]
-	}
-
-	return ResolvedPath{
-		PathName: strings.Join(paths, string(filepath.Separator)),
-		FileName: hashString,
-	}
 }
