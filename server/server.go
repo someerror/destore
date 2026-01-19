52726c122ba5f479dcca96a0f73bf27d1b6bdfc6
alex sync <koghuxov1990@gmail.com>
2026-01-19 17:58:40 +0300

    server

--- server/server.go
+++ server/server.go
@@ -12,7 +12,7 @@ type ServerOpts struct {
 	ListenAddr string
 	Store      core.Store
 	Transport  core.Transport
-	Logger *slog.Logger
+	Logger     *slog.Logger
 }
 
 type Server struct {
@@ -33,33 +33,53 @@ func NewServer(opts ServerOpts) (*Server, error) {
 }
 
 func (srv *Server) Start(ctx context.Context) error {
-	srv.Logger.Info("Server starting on", srv.ListenAddr)
+	srv.Logger.Info("Server starting on", "addr", srv.ListenAddr)
 
-	srv.Transport.ListenAndAccept()
-	
+	err := srv.Transport.ListenAndAccept()
+	if err != nil {
+		return err
+	}
+
 	srv.loop(ctx)
+
+	srv.Logger.Info("Server stopped")
+
+	return nil
 }
 
 func (srv *Server) loop(ctx context.Context) {
-	// srv.Transport.Close()
+	defer func() {
+		srv.Logger.Info("Shutting down transport")
+		srv.Transport.Close()
+	}()
+
 	select {
 	case msg := <-srv.Transport.Consume():
 		err := srv.handleMessage(msg)
-		if err!=nil {
-			srv.Logger.Error("Handling message error", err)
+
+		if err != nil {
+			srv.Logger.Error("Handling message", "error", err)
 
 			response := core.Message{
 				Payload: []byte(fmt.Sprintf("Server error: %q", err)),
 			}
 
-			if (msg.SourcePeer != nil) {
+			if msg.SourcePeer != nil {
 				msg.SourcePeer.Send(response)
 			}
 		}
+	case <-ctx.Done():
+		srv.Logger.Info("Context done")
+		return
 	}
 }
 
 func (srv *Server) handleMessage(msg core.Message) error {
-	fmt.Printf("message handled")
+	srv.Logger.Info("message handling")
+
+	if len(msg.Payload) == 0 {
+		return fmt.Errorf("empty payload received")
+	}
+
 	return nil
 }
